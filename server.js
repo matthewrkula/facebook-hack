a562222ae2556dc7c5f713d04d2a9ab7ac6ffd4f
Matt Kula <matthewrkula@gmail.com>
2013-11-09 20:24:47 -0600

    trying

--- server.js
+++ server.js
@@ -1,31 +1,29 @@
-// 192.17.207.254
-
 var app = require('http').createServer(handler)
   , io = require('socket.io').listen(app)
   , fs = require('fs')
 
 app.listen(8000);
 
-function handler (req, res){
+function handler (req, res) {
   fs.readFile(__dirname + req.url,
-    function(err, data){
-      if(err){
-        res.writeHead(500);
-        return res.end('Internal server error');
-      }
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
 
-      res.writeHead(200);
-      res.end(data);
-    });
+    res.writeHead(200);
+    res.end(data);
+  });
 }
 
-io.sockets.on('connection', function(socket){
+io.sockets.on('connection', function (socket) {
   socket.on('move', function(data){
-      socket.broadcast.emit(data.direction, { down: data.down });
+      socket.broadcast.emit(data.dir, { down: data.down });
   });
 
   socket.on('action-button', function(data){
-      socket.bradcast.emit(data.btn, { down: data.down });
+      socket.broadcast.emit(data.btn, { down: data.down });
   });
-
 });
+
