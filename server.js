d7e7a72ea71f568aea3243016011d33cbac0ad90
Matt Kula <matthewrkula@gmail.com>
2013-11-09 22:38:18 -0600

    Added scores as member of player class

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var app = require('http').createServer(handler)
   , io = require('socket.io').listen(app)
   , fs = require('fs')
 
-app.listen(80);
-// app.listen(8000);
+// app.listen(80);
+app.listen(8000);
 
 function handler (req, res) {
   fs.readFile(__dirname + req.url,
@@ -25,11 +25,11 @@ io.sockets.on('connection', function (socket) {
   });
 
   socket.on('move', function(data){
-      socket.broadcast.emit(data.dir, { down: data.down, id: data.id });
+      socket.broadcast.emit(data.dir, { pressed: data.pressed, id: data.id });
   });
 
   socket.on('action-button', function(data){
-      socket.broadcast.emit(data.btn, { down: data.down, id: data.id });
+      socket.broadcast.emit(data.btn, { pressed: data.pressed, id: data.id });
   });
 });
 
