e466a76957bc883227be8cdf66af763dd37a0da1
Matt Kula <matthewrkula@gmail.com>
2013-11-09 18:03:08 -0600

    Started node server

--- server.js
+++ server.js
@@ -1,1 +1,31 @@
 // 192.17.207.254
+
+var app = require('http').createServer(handler)
+  , io = require('socket.io').listen(app)
+  , fs = require('fs')
+
+app.listen(80);
+
+function handler (req, res){
+  fs.readFile(__dirname + req.url,
+    function(err, data){
+      if(err){
+        res.writeHead(500);
+        return res.end('Internal server error');
+      }
+
+      res.writeHead(200);
+      res.end(data);
+    });
+}
+
+io.sockets.on('connection', function(socket){
+  socket.on('move', function(data){
+      socket.broadcast.emit(data.direction, { down: data.down });
+  });
+
+  socket.on('action-button', function(data){
+      socket.bradcast.emit(data.btn, { down: data.down });
+  });
+
+});
