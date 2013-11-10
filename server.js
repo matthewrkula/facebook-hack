937b5e81b846f5cd57bf98dac5a612e2da3ccd76
Valentin Litvak <contact@vallitvak.com>
2013-11-10 09:32:18 -0600

    hopefully fixed the controls

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var app = require('http').createServer(handler)
   , io = require('socket.io').listen(app)
   , fs = require('fs');
 
-//var port = 80; // production
-var port = 8000; // Val/Matt development
+var port = 80; // production
+//var port = 8000; // Val/Matt development
 
 app.listen(port);
 
@@ -24,11 +24,11 @@ io.sockets.on('connection', function (socket) {
     socket.broadcast.emit('add-player', data);
   });
 
-  socket.on('move', function(data){
+  socket.on('move', function(data) {
       socket.broadcast.emit(data.dir, { pressed: data.pressed, id: data.id });
   });
 
-  socket.on('action-button', function(data){
+  socket.on('action-button', function(data) {
       socket.broadcast.emit(data.btn, { pressed: data.pressed, id: data.id });
   });
 });
