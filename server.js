364cb89876b6c708ebf25b7f6a134c35ecaf86f1
Matt Kula <matthewrkula@gmail.com>
2013-11-09 18:43:09 -0600

    Added controls and index

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = require('http').createServer(handler)
   , io = require('socket.io').listen(app)
   , fs = require('fs')
 
-app.listen(80);
+app.listen(8000);
 
 function handler (req, res){
   fs.readFile(__dirname + req.url,
